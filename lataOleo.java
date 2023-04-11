3743978c87cf5193b3800de2278606224b4f04b8
Beatriz Aimi <biagioni.aimi@gmail.com>
2023-04-11 15:14:53 -0300

    Add files via upload

---
+++ lataOleo.java
@@ -0,0 +1,14 @@
+package exercicios;
+
+public class lataOleo {
+	public class LataOleo {
+	    public static void main(String[] args) {
+	        double V, R, A;
+	        R = 3.2;
+	        A = 4.9;
+
+	        V = 3.14159 * R * R * A;
+	        System.out.println("O volume da lata de óleo é de " + V + " m³");
+	    }
+	}
+}
